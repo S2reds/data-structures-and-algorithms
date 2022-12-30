8e993de576098bb4b7a963f53692973d4e3de78c
S2reds <110210946+S2reds@users.noreply.github.com>
2022-12-29 19:29:16 -0800

    bruteforce

---
+++ twosum/bruteforce.ts
@@ -0,0 +1,7 @@
+function twoNumberSum(array: number[], targetSum: number): typeof array {
+    // Write your code here.
+    for (let i = 0; i < array.length - 1; i++) {
+        
+    }
+    return [-1, -1];
+  }
